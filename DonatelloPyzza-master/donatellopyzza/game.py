2d3e157bba50eabe89a492904d96cafbd3efb215
Nicolas <NT.Aergie@gmail.com>
2024-10-02 18:21:35 +0200

    test update ne sqare

---
+++ DonatelloPyzza-master/donatellopyzza/game.py
@@ -0,0 +1,75 @@
+from turtleAgent import Turtle
+from envBuilder import *
+
+import time
+import random
+from enum import Enum
+
+class Game:
+    def __init__(self, envName, gui):
+        self.envName = envName
+        self.gui = gui
+        self.env = None
+        pass
+
+    '''
+    Goal : start the game by initializing the environment
+    Return : a Turtle instance (a "user-friendly" wrapper class that includes one agent and the environment)
+    '''
+    def start(self):
+        # Instanciate the envBuilder
+        envbuilder = EnvBuilder(name=self.envName, gui=self.gui)
+        # creates the grid and the turtle
+        agent, env = envbuilder.build_grid()
+        self.env = env
+        turtle = Turtle(agent, env)
+        # make the turtle touch before starting the game to get a position
+        env.updatePosition(turtle.agent)
+        return turtle
+
+    """
+    Goal update only one square in the GUI (not the whole grid)    
+    """
+    def updateOneSquare(self, square):
+        self.env.updateOneSquare(square)
+
+    '''
+    Goal : get a dictionary of every non-wall squares with None values
+    Return : return the aforementioned dictionary
+    '''
+    def getSquaresDict(self):
+        return self.env.getSquaresAsDict()
+
+    '''
+    Goal : modify the color of each non-wall square
+    Param : colors - a dictionary with keys (x, y) and pygame.Color as values
+    '''
+    def setSquaresColors(self, colors):
+        self.env.setSquaresColors(colors)
+
+    '''
+    Goal : modify the text of each non-wall square
+    Param : texts - a dictionary with keys (x, y) and string as values
+    '''
+    def setSquaresText(self, texts):
+        self.env.setSquaresText(texts)
+
+    '''
+    Goal : start the game by initializing the environment
+    Return : True if the turtle found the pizza, else False
+    '''
+    def isWon(self, prnt=True):
+        res = self.env.pizzaIsFound()
+        if res and prnt:
+            print("-----------------------------------------------------")
+            print("###                  YOU WIN                      ###")
+            print("-----------------------------------------------------")
+            print("Pizza has been found with", self.env.nbActions, "actions !")
+        return res
+
+    def getScore(self):
+        return self.env.nbActions
+        
+
+
+
---
+++ DonatelloPyzza-master/examples/changing_color.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("donatellopyzza/"))
+from game import *
+
+from random import randint
+import pygame
+import time
+from enums import Action
+
+if __name__ == '__main__':
+    __ENVIRONMENT__ = "maze"
+    __GUI__ = True
+
+    game = Game(__ENVIRONMENT__, __GUI__)
+    turtle = game.start()
+    
+    while not game.isWon():
+        actions = [Action.MOVE_FORWARD, Action.MOVE_FORWARD, Action.TURN_RIGHT,
+                   Action.MOVE_FORWARD, Action.MOVE_FORWARD, Action.MOVE_FORWARD]
+        for a in actions:
+            squares = game.getSquaresDict()
+
+            
+
+            # Définir les couleurs des carrés
+            for key in squares:
+                r = randint(0, 255)
+                g = randint(0, 255)
+                b = randint(0, 255)
+                squares[key] = pygame.Color(r, g, b, 255)
+            game.setSquaresColors(squares)
+
+            # Définir les textes des carrés
+            texts = {}
+            for key in squares:
+                
+                texts[key] = f"Pos {key}"  # Exemple : afficher les coordonnées du carré
+            game.setSquaresText(texts)
+
+            game.updateOneSquare((2, 2))
+            
+            result, _ = turtle.execute(a)
+            time.sleep(1)
